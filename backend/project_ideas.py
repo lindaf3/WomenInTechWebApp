5a45d7c4d00958b0b3e892563e604368c45c7bad
Linda Fan <lindaf3@uci.edu>
2021-04-25 00:11:33 -0700

    added project ideas
    Co-authored-by: Vikasni Kalahasthi <maplefries@users.noreply.github.com>

--- backend/project_ideas.py
+++ backend/project_ideas.py
@@ -30,11 +30,19 @@ def down_vote(chron_posts, pid):
     chron_posts[pid].downvote()
     return sorted(chron_posts, key=operator.attrgetter('votes'), reverse=True)
 
-# chrp = []
-# p = add_project_idea(chrp, "asdlfkjsldkf", "post1")
-# print(p)
-# p = add_project_idea(chrp, "adfsdfasdfasdfasd", "post2")
-# print(p)
+chrp = []
+p = add_project_idea(chrp, "Build a website about your favorite TV series", "Bob Billy")
+p = add_project_idea(chrp, "Build an android app based on a simple to-do list", "Bill Bobby")
+p = add_project_idea(chrp, "Build a website about your favorite TV series", "Kaela Kealoha")
+p = add_project_idea(chrp, "Build a website with the Spotify API", "Kiana Kealoha")
+p = add_project_idea(chrp, "Build yourself a weather app!", "Sabrina Capello")
+p = add_project_idea(chrp, "Create a website to teach people about sustainability", "Chanda Misra")
+p = add_project_idea(chrp, "Make a video game!", "Billy Bob")
+p = add_project_idea(chrp, "Make a health app to stay fit!","Reckless Man")
+p = add_project_idea(chrp,"Location tracker for when you lose your friends","Turtle Boy")
+p = add_project_idea(chrp,"Build yourself a virtual paint wheel for color mixing","Chalk Prince")
+p = add_project_idea(chrp,"Build an app to help you invest money and get rich","Eclipse Star")
+
 # p = down_vote(chrp, 0)
 # p = up_vote(chrp, 0)
 # print(p)
